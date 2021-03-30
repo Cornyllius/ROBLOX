3ac242d33fe5c8b62afd110ca89302c27578ef83
Cornyllius <53982126+Cornyllius@users.noreply.github.com>
2021-03-30 19:07:11 +0200

    Update hcbbkeys.lua

--- ignore/hcbbkeys.lua
+++ ignore/hcbbkeys.lua
@@ -1,4 +1,1 @@
-local keys = {
-	a489748945612165145611 = "591bcf7ddf7ed5e2ae445b80cfb68af61c33e838d93b71ba26ec626bb3b4f25b64c2ecc3d99ff54dcfdc956c0ef9ba21fcb8c291a518702c3ce7e1d198d7e469"
-}
-return keys
+
