a92b8b50b62f1a8bf9ded8ca4638f72ea4c5961c
Cornyllius <53982126+Cornyllius@users.noreply.github.com>
2021-04-16 15:10:15 +0200

    Create click tp.lua

---
+++ Apocalypse Rising/click tp.lua
@@ -0,0 +1,17 @@
+repeat wait() until game:GetService("Players").LocalPlayer ~= nil and game:GetService("Players").LocalPlayer.Character ~= nil
+local TS = game:GetService("TweenService")
+local player = game:GetService("Players").LocalPlayer
+local UIS = game:GetService("UserInputService")
+local Mouse = player:GetMouse()
+
+local function tp(x, y, z)
+    local info = TweenInfo.new(0.1)
+    local move = TS:Create(player.Character.HumanoidRootPart, info, {CFrame = CFrame.new(x, y, z)})
+    move:Play()
+end
+
+UIS.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        tp(Mouse.Hit.X,Mouse.Hit.Y+3,Mouse.Hit.Z)
+    end
+end)
