a904e12f1f0b5d48168b86826aedc7c40d740126
Cornyllius <53982126+Cornyllius@users.noreply.github.com>
2021-04-17 23:16:32 +0200

    Create Caliber Gun Mods.lua

---
+++ Caliber Gun Mods.lua
@@ -0,0 +1,28 @@
+-- Made by Blissful#4992, u can ass stuff to this here's template a1 = {'VARIABLE', 0}
+local list = {
+    str1 = {'RecoilMult', 0},
+    str2 = {'ReloadTime', 0},
+    str3 = {'Spread', 0},
+    str4 = {'Automatic', true},
+    str5 = {'Range', 1000000},
+    str6 = {'Ammo', 1000000},
+    str7 = {'ClipSize', 1000001},
+    str8 = {'FireRate', 0.05},
+    str9 = {'EquipTime', 0}
+}
+
+local function Mod(lib, str)
+    if type(lib) == 'table' and rawget(lib, str[1]) then
+        coroutine.wrap(function()
+            while wait() do
+                lib[str[1]] = str[2]
+            end
+        end)()
+    end
+end
+
+for i, v in pairs(getgc(true)) do
+    for u, x in pairs(list) do
+        Mod(v, x)
+    end
+end
