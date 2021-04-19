de485a71a1e24a0db4a9b771b28f038636af4566
Cornyllius <53982126+Cornyllius@users.noreply.github.com>
2021-04-19 22:54:16 +0200

    Create RO-Wizard Wand.lua

---
+++ RO-Wizard Wand.lua
@@ -0,0 +1,27 @@
+spawn(function()
+    for i, v in next, getgc(true) do
+        if type(v) == 'table' and rawget(v, 'Gems') then
+            v.Gems = 0
+        end
+    end
+    for i, v in next, getgc(true) do
+        if type(v) == 'table' and rawget(v, 'Damage') then
+            coroutine.wrap(function()
+                while wait(0.1) do
+                    v.ShakeIntensity = 0
+                    v.Damage = 100
+                    v.Speed = 1000
+                    v.UnloadAfterCast = false
+                    v.Cooldown = 0
+                    v.Safe = true
+                    v.ChargeCooldown = 0
+                    v.Charges = math.huge
+                    v.MaxCharges = math.huge
+                    if v.Duration then
+                        v.Duration = math.huge
+                    end
+                end
+            end)()
+        end
+    end
+end)
