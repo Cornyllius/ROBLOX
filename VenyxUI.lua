bffd6df796f78387274812d24edd819f3fffb95e
Cornyllius <53982126+Cornyllius@users.noreply.github.com>
2021-04-25 21:55:43 +0200

    Update VenyxUI.lua

--- VenyxUI.lua
+++ VenyxUI.lua
@@ -188,7 +188,8 @@ do
 		input.InputChanged:Connect(function(input)
 			if input == dragInput and dragging then
 				local delta = input.Position - mousePos
-				parent.Position  = UDim2.new(framePos.X.Scale, framePos.X.Offset + delta.X, framePos.Y.Scale, framePos.Y.Offset + delta.Y)
+                local smooth = tween:Create(parent, TweenInfo.new(0.1), {Position = UDim2.new(framePos.X.Scale, framePos.X.Offset + delta.X, framePos.Y.Scale, framePos.Y.Offset + delta.Y)})
+				--parent.Position = UDim2.new(framePos.X.Scale, framePos.X.Offset + delta.X, framePos.Y.Scale, framePos.Y.Offset + delta.Y)
 			end
 		end)
 
