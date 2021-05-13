ee90c08971b57fd623fea78eae541d85401c4cd2
Blissful <53982126+Blissful4992@users.noreply.github.com>
2021-05-13 14:24:11 +0200

    Update tnf.lua

--- Hawk/tnf.lua
+++ Hawk/tnf.lua
@@ -52,7 +52,7 @@ local function ESP(v, counter, type)
         local function Update()
             local c 
             c = game:GetService("RunService").RenderStepped:Connect(function()
-                if args[3] == true then
+                if game.CoreGui:FindFirstChild("FinityUI") == false then
                     NAME:Remove()
                     c:Disconnect()
                 end
@@ -87,7 +87,7 @@ local function ESP(v, counter, type)
         local function Update()
             local c 
             c = game:GetService("RunService").RenderStepped:Connect(function()
-                if args[3] == true then
+                if game.CoreGui:FindFirstChild("FinityUI") == false then
                     NAME:Remove()
                     c:Disconnect()
                 end
