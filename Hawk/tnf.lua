b630dd47c66b1f9a8b0fef21adf4fae19a5ebebd
Blissful <53982126+Blissful4992@users.noreply.github.com>
2021-05-13 10:40:52 +0200

    Update tnf.lua

--- Hawk/tnf.lua
+++ Hawk/tnf.lua
@@ -1,2 +1,12 @@
 local args = {...}
-print(args[1])
+local GameCategory = args[1]:Category(Current_Game.Name)
+
+local GameVisuals = GameCategory:Sector("Visuals")
+
+local GameVisual_Settings = {
+    Item_ESP = false
+}
+
+GameVisuals:Cheat("Checkbox", "Item ESP", function (NewValue)
+    GameVisual_Settings.Item_ESP = NewValue
+end, {enabled = GameVisual_Settings.Item_ESP})
