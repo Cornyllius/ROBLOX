0e52eb321fe9224078016a0893934310e360d515
Blissful <53982126+Blissful4992@users.noreply.github.com>
2022-03-05 11:12:32 +0100

    Create smallest.lua

---
+++ smallest.lua
@@ -0,0 +1,35 @@
+local pageLimit = math.huge -- Set to math.huge to explore all pages
+
+------------------------------------------------------------
+
+local HttpService = game:GetService('HttpService');
+local TPService = game:GetService("TeleportService");
+
+local nextCursor, serverId;
+local minimum = math.huge;
+
+local Page = 0;
+repeat
+    local Url = "https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Asc&limit=100";
+    if (nextCursor) then Url = Url.."&cursor="..nextCursor end;
+    local Servers = HttpService:JSONDecode(game:HttpGet(Url));
+
+    if Servers then
+        nextCursor = Servers.nextPageCursor or nil;
+        Page = Page + 1;
+        for _,v in pairs(Servers.data) do
+            v.playing = v.playing or math.huge;
+            v.id = v.id or '';
+
+            if v.id ~= game.JobId and v.playing <= minimum then -- Smaller OR Equal to get the LAST + SMALLEST server
+                minimum = v.playing;
+                serverId = v.id;
+            end
+        end
+    end
+until (not nextCursor) or (Page >= pageLimit);
+
+if (serverId) then -- Teleport
+    warn("Teleporting to:", tostring(serverId));
+    TPService:TeleportToPlaceInstance(game.PlaceId, serverId);
+end
